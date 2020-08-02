d6ff94c25cbba70d9a46f5071b7a58e001f8acda
20986tsuruma <toshinori1202@gmail.com>
2020-08-02 17:27:25 +0900

    [Finish] bookers1_debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @books = Book.new
   end
 
   def create
@@ -14,6 +15,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "successfully created book!"
     else
+      @books = Book.all
       render 'index'
     end
   end
@@ -22,7 +24,7 @@ class BooksController < ApplicationController
   end
 
   def update
-    if @book.update()
+    if @book.update(book_params)
       redirect_to book_path(@book), notice: "successfully updated book!"
     else
       render 'edit'
